405ce58a23a55bc308f47bbadaa7c23a2ad8c029
Maxim <xmaksutx@gmail.com>
2022-08-14 00:14:52 +0200

    Initial commit

---
+++ lesson1/hello_cargo/target/debug/deps/hello_cargo-7a2ed52c79147bd6.d
@@ -0,0 +1,5 @@
+/home/max/IdeaProjects/Rust_lessons/lesson1/hello_cargo/target/debug/deps/hello_cargo-7a2ed52c79147bd6: src/main.rs
+
+/home/max/IdeaProjects/Rust_lessons/lesson1/hello_cargo/target/debug/deps/hello_cargo-7a2ed52c79147bd6.d: src/main.rs
+
+src/main.rs:
---
+++ lesson1/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/max/IdeaProjects/Rust_lessons/lesson1/hello_cargo/target/debug/hello_cargo: /home/max/IdeaProjects/Rust_lessons/lesson1/hello_cargo/src/main.rs
